39d80894f18feeb1a9fb5344417d855ba9e7d702
Prischepa Igor <i.a.prishepa@gmail.cokm>
2023-02-27 09:25:29 +0300

    add required attribute

--- tfm.api.bll/DTO/NewStyleDto.cs
+++ tfm.api.bll/DTO/NewStyleDto.cs
@@ -1,7 +1,10 @@
-﻿namespace tfm.api.bll.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tfm.api.bll.DTO
 {
     public sealed class NewStyleDto
     {
+        [Required]
         public string StyleName { get; set; } = null!;
     }
 }
