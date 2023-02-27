a0808901fe3f9025f4d0a656d814819fc8fde623
Prischepa Igor <i.a.prishepa@gmail.cokm>
2023-02-27 08:56:56 +0300

    Add role repo

---
+++ tfm.api.dal/Repos/Contracts/IStyleRepo.cs
@@ -0,0 +1,11 @@
+﻿using tfm.api.dal.Entities;
+
+namespace tfm.api.dal.Repos.Contracts
+{
+    public interface IStyleRepo
+    {
+        Task<int> AddAsync(Role newStyle);
+
+        Task DeleteAsync(int roleId);
+    }
+}
