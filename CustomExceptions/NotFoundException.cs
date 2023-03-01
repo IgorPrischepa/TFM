24330d7766e51049462515f45adad638362cbd56
Igor Prischepa <5076626+IgorPrischepa@users.noreply.github.com>
2023-03-01 15:14:03 +0300

    Cleanup

--- CustomExceptions/NotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CustomExceptions
-{
-    public sealed class NotFoundException : Exception
-    {
-        public NotFoundException(string? message) : base(message)
-        {
-        }
-    }
-}
