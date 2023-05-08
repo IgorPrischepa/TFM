c8a1e1c3950db842e948e4bdeef92ad960b76a97
Igor Prischepa <5076626+IgorPrischepa@users.noreply.github.com>
2023-05-09 02:43:05 +0300

    fix missconfig model

---
+++ tfm.api.dal/Migrations/20230508234203_MakePassRequired.cs
@@ -0,0 +1,500 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace tfm.api.dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakePassRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Examples_PhotoFiles_PhotoFileId",
+                table: "Examples");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Examples_Styles_StyleStyleId",
+                table: "Examples");
+
+            migrationBuilder.DropTable(
+                name: "RoleUser");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Masters_UserId",
+                table: "Masters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Examples_PhotoFileId",
+                table: "Examples");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Users",
+                newName: "MiddleName");
+
+            migrationBuilder.RenameColumn(
+                name: "StyleStyleId",
+                table: "Examples",
+                newName: "StyleId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Examples_StyleStyleId",
+                table: "Examples",
+                newName: "IX_Examples_StyleId");
+
+            migrationBuilder.RenameColumn(
+                name: "Phone",
+                table: "Contacts",
+                newName: "Value");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ExampleId",
+                table: "PhotoFiles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "PhotoFiles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBlocked",
+                table: "Masters",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortDescription",
+                table: "Examples",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AvatarId",
+                table: "Customers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Contacts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Contacts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "RoleEntityUserEntity",
+                columns: table => new
+                {
+                    RolesId = table.Column<int>(type: "integer", nullable: false),
+                    UsersId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleEntityUserEntity", x => new { x.RolesId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_RoleEntityUserEntity_Roles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoleEntityUserEntity_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedule",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DayOfWeek = table.Column<int>(type: "integer", nullable: false),
+                    StartTime = table.Column<TimeOnly>(type: "time without time zone", nullable: false),
+                    EndTime = table.Column<TimeOnly>(type: "time without time zone", nullable: false),
+                    MasterId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedule", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Schedule_Masters_MasterId",
+                        column: x => x.MasterId,
+                        principalTable: "Masters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScheduleBlockers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StartDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    MasterId = table.Column<int>(type: "integer", nullable: false),
+                    Reason = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleBlockers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ScheduleBlockers_Masters_MasterId",
+                        column: x => x.MasterId,
+                        principalTable: "Masters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Admin" },
+                    { 2, "Customer" },
+                    { 3, "Master" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_Name",
+                table: "Roles",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PhotoFiles_ExampleId",
+                table: "PhotoFiles",
+                column: "ExampleId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PhotoFiles_UserId",
+                table: "PhotoFiles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Masters_UserId",
+                table: "Masters",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_AvatarId",
+                table: "Customers",
+                column: "AvatarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleEntityUserEntity_UsersId",
+                table: "RoleEntityUserEntity",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedule_MasterId",
+                table: "Schedule",
+                column: "MasterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleBlockers_MasterId",
+                table: "ScheduleBlockers",
+                column: "MasterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_PhotoFiles_AvatarId",
+                table: "Customers",
+                column: "AvatarId",
+                principalTable: "PhotoFiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Examples_Styles_StyleId",
+                table: "Examples",
+                column: "StyleId",
+                principalTable: "Styles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PhotoFiles_Examples_ExampleId",
+                table: "PhotoFiles",
+                column: "ExampleId",
+                principalTable: "Examples",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PhotoFiles_Users_UserId",
+                table: "PhotoFiles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_PhotoFiles_AvatarId",
+                table: "Customers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Examples_Styles_StyleId",
+                table: "Examples");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PhotoFiles_Examples_ExampleId",
+                table: "PhotoFiles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PhotoFiles_Users_UserId",
+                table: "PhotoFiles");
+
+            migrationBuilder.DropTable(
+                name: "RoleEntityUserEntity");
+
+            migrationBuilder.DropTable(
+                name: "Schedule");
+
+            migrationBuilder.DropTable(
+                name: "ScheduleBlockers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Roles_Name",
+                table: "Roles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PhotoFiles_ExampleId",
+                table: "PhotoFiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PhotoFiles_UserId",
+                table: "PhotoFiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Masters_UserId",
+                table: "Masters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_AvatarId",
+                table: "Customers");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ExampleId",
+                table: "PhotoFiles");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "PhotoFiles");
+
+            migrationBuilder.DropColumn(
+                name: "IsBlocked",
+                table: "Masters");
+
+            migrationBuilder.DropColumn(
+                name: "AvatarId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Contacts");
+
+            migrationBuilder.RenameColumn(
+                name: "MiddleName",
+                table: "Users",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "StyleId",
+                table: "Examples",
+                newName: "StyleStyleId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Examples_StyleId",
+                table: "Examples",
+                newName: "IX_Examples_StyleStyleId");
+
+            migrationBuilder.RenameColumn(
+                name: "Value",
+                table: "Contacts",
+                newName: "Phone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortDescription",
+                table: "Examples",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Avatar",
+                table: "Customers",
+                type: "bytea",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "RoleUser",
+                columns: table => new
+                {
+                    RolesId = table.Column<int>(type: "integer", nullable: false),
+                    UsersId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleUser", x => new { x.RolesId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_RoleUser_Roles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoleUser_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Masters_UserId",
+                table: "Masters",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Examples_PhotoFileId",
+                table: "Examples",
+                column: "PhotoFileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleUser_UsersId",
+                table: "RoleUser",
+                column: "UsersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Examples_PhotoFiles_PhotoFileId",
+                table: "Examples",
+                column: "PhotoFileId",
+                principalTable: "PhotoFiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Examples_Styles_StyleStyleId",
+                table: "Examples",
+                column: "StyleStyleId",
+                principalTable: "Styles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
